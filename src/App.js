464dad27f487bc061862f838fd6ee45ac59cb3d3
Daniel Batista <dani_batista20@hotmail.com>
2022-08-05 02:51:14 +0100

    start: globals & custom styles | add: jsconfig.json | test: styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.scss';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>hello</h1> <br />
+      <p>hello world</p>
+      <button className="btn">Link</button>
+      <button className="btn btn-small">Link Small</button>
+      <button className="btn btn-large">Link</button>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora debitis
+        mollitia aliquam hic laboriosam iure nam ipsum reiciendis accusamus!
+        Voluptatum quidem facilis, perferendis cumque dignissimos eligendi minus
+        illo officia modi, ipsa natus labore officiis quaerat. Totam dolorum
+        iste odio alias ipsa minima dolores deserunt iusto facilis tempore
+        vitae, quae consequatur, quisquam voluptatibus molestias atque obcaecati
+        natus, rem provident pariatur qui incidunt. Itaque totam tempore velit
+        cum suscipit. At maiores perferendis nihil laboriosam quas tenetur
+        molestias in aspernatur facilis quis blanditiis, doloribus minus
+        pariatur architecto dicta, rerum corporis distinctio error, cupiditate
+        temporibus voluptas sint nisi. Dolorem alias reprehenderit iusto. Fuga,
+        beatae numquam, quam laudantium rem officiis, magnam ea modi repudiandae
+        excepturi assumenda odit harum esse at? Aliquid natus cum reprehenderit
+        aliquam a nisi accusamus. Repellendus quasi, sequi dignissimos a
+        quisquam culpa impedit quibusdam quidem earum cupiditate enim
+        praesentium, omnis molestiae sapiente optio quo ipsa eius repudiandae
+        nulla placeat ad numquam dicta esse. Odit consectetur voluptatibus eius
+        unde, earum velit explicabo fugiat quis excepturi. Quis, dolore
+        cupiditate? Ipsa neque consequuntur labore libero quae. Eaque, est. Ea a
+        facere ex, explicabo amet voluptates laborum nostrum nam eligendi fugiat
+        at tempora, commodi esse possimus soluta rem neque exercitationem error
+        ullam cumque eveniet qui fugit quasi ratione! Deserunt deleniti tempora
+        incidunt iusto expedita exercitationem. Sint quaerat id repudiandae
+        impedit magnam similique error qui quis velit in, enim ut placeat
+        blanditiis vel harum dolore eum accusamus. Nisi reiciendis dicta dolore
+        ipsam numquam cum impedit omnis, mollitia nostrum deleniti maxime rem
+        perspiciatis vel quae quam, debitis beatae reprehenderit. Necessitatibus
+        corrupti nemo reiciendis dolorem adipisci doloremque dolores hic veniam
+        vero maiores, quasi itaque, nihil cum dolor id voluptatem earum
+        eligendi? Consequatur at excepturi illo cumque ipsum accusantium sed!
+        Suscipit, error odio ex architecto aperiam doloremque, facere molestias
+        quam sapiente nesciunt veritatis reiciendis qui tempora distinctio
+        corrupti enim ratione reprehenderit quo natus sunt? Ex at, voluptate
+        minima eos voluptas voluptates facere commodi aliquam facilis placeat
+        magni error! Sunt neque modi quis est facere vel quidem odio minus
+        eligendi blanditiis animi pariatur maxime sint, recusandae veniam
+        nostrum consectetur corrupti sapiente praesentium aliquid amet alias
+        perspiciatis. Dolor repellendus quae consequatur pariatur odio adipisci
+        numquam amet dolorem blanditiis enim. Ratione laudantium consequatur cum
+        maxime quas, nihil sunt praesentium architecto incidunt, nulla placeat
+        at ea. Excepturi autem dicta non exercitationem dolores, doloribus
+        itaque accusantium officiis quis natus suscipit? Praesentium soluta
+        dolore libero, eaque recusandae ut aliquam quia, fugiat iste repellat
+        beatae ipsa enim. Dolore delectus aliquid ad voluptate consectetur,
+        vitae debitis minima, provident laudantium totam accusamus! Incidunt,
+        aliquid. Iste provident laboriosam nobis impedit dolorum. Voluptas
+        doloremque magni eius, illo deserunt saepe sint sunt. Incidunt
+        perferendis similique magnam commodi suscipit, quidem doloremque ullam
+        labore consequatur saepe ad consequuntur quaerat voluptatem nemo
+        molestiae illum impedit tenetur eos quo molestias quia temporibus
+        inventore odit natus. Molestiae dignissimos mollitia modi odit facere
+        veritatis laudantium qui porro eligendi! Cumque eaque ab sit quis
+        pariatur, labore ipsa consequuntur, saepe sapiente exercitationem beatae
+        nisi? Fuga minus vel quisquam? Ex, provident vero veritatis ipsa magni
+        tempora. Reiciendis eveniet id ullam repudiandae!
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

63e571213e0b48dc514979d6735a5cc330300a24
Daniel Batista <dani_batista20@hotmail.com>
2022-08-08 02:09:27 +0100

    upd: UserDetail - useGetUserReposQuery() isLoading, isSuccess

--- src/features/github/UserDetail.js
+++ src/features/github/UserDetail.js
@@ -1,19 +1,29 @@
 import React, { Fragment } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import classes from 'styles/github/UserDetail.module.scss'
+import { useGetUserReposQuery } from 'features/api/githubApi'
 import { useSelector } from 'react-redux'
 import NoUser from './user/NoUser'
 import Profile from './user/Profile'
 import Repositories from './user/Repositories'
+import Loading from 'components/ui/Loading'
 
 const UserDetail = (props) => {
   //
   const user = useSelector((state) => state.github.users[0])
   const params = useParams()
   const { userId } = params
-  const repository = {}
+
+  const {
+    data: repos,
+    isLoading,
+    isSuccess,
+    // isError,
+    // error
+  } = useGetUserReposQuery(user.login)
 
   let content
+
   // if no user has been searched
   if (user.id === 0 || user.id !== Number(userId)) {
     content = <NoUser />
@@ -23,9 +33,10 @@ const UserDetail = (props) => {
     content = (
       <Fragment>
         <h3>A page to display the user information and repositories. </h3>
-        <div className={classes.details}>
+        <div className={`${classes.details} centered`}>
           <Profile user={{ ...user }} />
-          <Repositories repository={{ ...repository }} />
+          {isLoading && <Loading />}
+          {isSuccess && <Repositories repos={repos} />}
         </div>
         <p>
           <Link className="btn" to="/search">

d4d6f99c2e61671e42852b5ebb76acf61ed71609
Daniel Batista <dani_batista20@hotmail.com>
2022-08-08 02:10:47 +0100

    upd: Repositories - render repos list

--- src/features/github/user/Repositories.js
+++ src/features/github/user/Repositories.js
@@ -2,19 +2,19 @@ import React from 'react'
 import classes from 'styles/github/user/Repositories.module.scss'
 
 const Repositories = (props) => {
-  // const { name } = props.repository
+  const repos = props.repos
+  // console.log(repos);
   return (
     <aside className={`${classes.repositories} text-center`}>
-      <h4 className='title'>User Repositories List</h4>
+      <h4 className="title">User Repositories List</h4>
       <div className={classes.info}>
-      <dl>
-        <dt>Nome_nome</dt>
-        <dd>descrição do projecto</dd>
-      </dl>
-      <dl>
-        <dt>Nome</dt>
-        <dd>descrição do projecto</dd>
-      </dl></div>
+        {repos.map((repo) => (
+          <dl key={repo.id}>
+            <dt>{repo.name}</dt>
+            <dd>{repo.description ? repo.description : "*no-description*" }</dd>
+          </dl>
+        ))}
+      </div>
     </aside>
   )
 }
